a14798885866de68eeb76e1a37b4f921136f3d0f
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2023-11-24 23:13:45 +0630

    Change the quote in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     print("Everything work out correctly")
-    print(f"Env value of AGE is {os.environ["AGE"]}")
+    print(f"Env value of AGE is {os.environ['AGE']}")
 
 
 if __name__=="__main__":
